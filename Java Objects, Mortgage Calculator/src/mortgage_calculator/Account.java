67e54541d1323eb5b1b0b580cdf371c48f7395c0
reynoldsqa@student.swosu.edu <reynoldsqa@student.swosu.edu>
2019-02-04 13:33:41 -0600

    added intial works

---
+++ Java Objects, Mortgage Calculator/src/mortgage_calculator/Account.java
@@ -0,0 +1,17 @@
+package mortgage_calculator;
+
+public class Account {
+ 
+	Account Account = new Account();
+//Declare all variables
+		private double id = 0; 
+		private double balance = 0;
+		private double annunalInterestRate = 0;
+		private int dateCreated = 0;
+ 
+ this.id = 1;
+ this.balance = 1;
+ this.annualIntrestRate = 0;
+ this.dateCreated = 00119;
+ 
+}
