825c2c718ed57970023543532958e35d5fdc5d84
LAB <LAB>
2019-02-15 13:39:40 -0600

    started cointoss

---
+++ Coin Toss/src/cointoss/CoinTossTester.java
@@ -0,0 +1,12 @@
+package cointoss;
+
+public class CoinTossTester {
+
+	public static void main(String[] args) {
+		System.out.println("Testing Please Ignore");
+		
+		CoinClass coin = new CoinClass();
+
+	}
+
+}
