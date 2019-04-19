7be10a7ce3468c133d057b2d0be60991b66e162f
Jeremy Evert <jeremy.evert@swosu.edu>
2019-04-19 12:56:06 -0500

    finished basic secotr 1

--- Racing Unicorns/src/unicornisland/Race.java
+++ Racing Unicorns/src/unicornisland/Race.java
@@ -1,29 +1,35 @@
-package unicornisland;
+
 
 import java.util.Random;
 
 public class Race {
 
 	public static void main(String[] args) {
+
 		System.out.println("Testing Please Ignore");
-		int iterationsector1= 5
+		int iterationsector1= 5;
 		int unicornspeed = 1;
-		System.out.println("time in sector one is " + sector1(unicornspeed));
+		double TyreEfficency = 2;
+
+		System.out.println("time in sector one is " +
+		sector1(unicornspeed, iterationsector1, TyreEfficency));
 
 
 	}
 
-	public static int sector1(int unicornspeed, int iterationsector1) {
+	public static double sector1(int unicornspeed,
+			int iterationsector1, double tyreEfficency) {
 		Random rand = new Random();
-		int currentiterations = 0
-		while  (int iterationsector1 < currentiterations)
-		{
+		int currentiterations = 0;
+		double realtivecurve = 0.03;
+		//	while  (int iterationsector1 < currentiterations)
+		//	{
 
 		int speed = (int) (rand.nextInt(15) + unicornspeed);
 
- 		System.out.println("Speed in Sector one is " + speed);
-		currentiterations++
-		}
+		System.out.println("Speed in Sector one is " + speed);
+		//		currentiterations++
+		//		}
 
 		if(speed < 5){
 
@@ -34,7 +40,10 @@ public class Race {
 			System.out.println("Solid sector");
 		}
 
-		int timesector1 = 1000/speed;
+		double timesector1 =
+				(double)1000/(double)((double)speed * tyreEfficency);
+
+		tyreEfficency = tyreEfficency - realtivecurve;
 
 		return timesector1;
 
